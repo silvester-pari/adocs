4811e038e39aa2c82fee4125e1b431eab0bc4846
Silvester <silvester.pari@eox.at>
2024-04-23 17:50:56 +0200

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import "./custom.css";
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
+  async enhanceApp({ app }) {
     if (!import.meta.env.SSR) {
       const plugin = await import('@eox/map')
       app.use(plugin)
